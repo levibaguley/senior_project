b3579d2b9461bb6ecd5891ddf883b7927a6e6b12
levibaguley <levi.baguley@gmail.com>
2020-05-11 22:44:37 -0600

    finding confidence intervals

--- first_exploration.R
+++ first_exploration.R
@@ -1,5 +1,7 @@
 library(tidyverse)
-library(tidymodels)
+#library(tidymodels)
+library(parsnip)
+
 
 sample <-
   read_csv("radarwindmillsampledata.csv" %>% here::here()) %>%
@@ -11,29 +13,50 @@ sample <-
              factor(levels = c("not_present", "present")),
          .keep = "unused")
 
-prob_root <- function(range, turbines, reg_fit, prob = .5) {
-  coefs <- reg_fit$fit %>% coef() %>% unname()
+#' Helper fuction for finding R_50 from the logistic model
+#' @param range The range of the target. Main input for `uniroot()`
+#' @param turbines Are there turbinds? 1/0 or T/F
+#' @param log_fit The fit of the regression as a parsnip model object
+#' @param prob The probability that determines where the root is and
+#' the corresponding `range` that will be found in `getR_50()`
+#' therefore the default is .5.
+prob_root <- function(range, turbines, log_fit, prob = .5) {
+  coefs <- log_fit$fit %>% coef() %>% unname()
   return(1 /
            (1 + exp(-(coefs[1] + coefs[2] * range +
                         coefs[3] * turbines)))
         - prob)
 }
 
-getR_50 <- function(fit) {
-  map_dbl(0:1,
-          ~ uniroot(prob_root,
-                    interval = range(fit$fit$data$range),
-                    reg_fit = fit,
-                    turbines = .x) %>%
-            `$`(root)) %>%
-    set_names(c("R_50.not_present", "R_50.present"))
+#' Finds R_50 for when turbines are not_present and present
+#' @param log_fit The fit of the regression as a parsnip model object
+getR_50 <- function(fit, prob=.5) {
+
+  R_50 <- map_dbl(F:T,
+                  ~ uniroot(prob_root,
+                            interval = range(fit$fit$data$range),
+                            prob = prob,
+                            log_fit = fit,
+                            turbines = .x) %>%
+                    `$`(root))
+  return(tibble(turbines= c("not_present", "present"),
+                range = R_50))
 }
 
-logistic_reg() %>%
+
+R_50_mod <- logistic_reg() %>%
   set_engine("glm") %>%
-  fit(detection ~ range + turbines,
-      data=sample) %>%
+  fit(detection ~ range + turbines, data=sample)
+
+R_50_tibble <- R_50_mod %>%
   getR_50()
+
+
+R_50_mod %>%
+  predict.model_fit(R_50_tibble,
+                    type = 'conf_int')  %>%
+  # select(.pred_lower_detected, .pred_upper_detected) %>%
+  mutate(lower = getR_50(R_50_mod, .pred_lower_detected))
 
 
 sample %>%
@@ -45,8 +68,12 @@ ggplot(aes(as.numeric(detection =="detected"), range, color = turbines)) +
   theme_bw()
 
 
-
-
+sample %>%
+  ggplot(aes(range,as.numeric(detection =="detected"), color = turbines)) +
+  geom_jitter(height = .02) +
+  geom_smooth(method = "glm",
+              method.args = list(family = "binomial")) +
+  theme_bw()
 
 
 
