7cab0b5beb584bc75fc59d5f7000e11173b7f638
levibaguley <levi.baguley@gmail.com>
2020-05-22 17:10:01 -0600

    solved conf intervals

---
+++ R50.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(parsnip)
+library(magrittr)
+
+source(here::here("R50_functions.R"))
+
+sample <-
+  read_csv(here::here("radarwindmillsampledata.csv")) %>%
+  rename(detection = response,
+         turbines = windmills)
+
+R50_mod <- logistic_reg() %>%
+  set_engine("glm") %>%
+  fit(as.factor(detection) ~ range + turbines, data = sample)
+
+R50_dat <-
+  tibble(turbines = 0:1) %>%
+  mutate(R50 = range_from_prob(0.5, turbines, R50_mod)) %>%
+  left_join(solve_R50_conf(R50_mod, res = 1000), by = "turbines")
+
+plot_dat <-
+  tibble(turbines = c(rep(0, 1000), rep(1, 1000))) %>%
+  mutate(range = rep(seq(0, 40, length.out = 1000), 2)) %>%
+  bind_cols(
+    predict(R50_mod, new_data = ., type = 'prob'),
+    predict(R50_mod, new_data = ., type = 'conf_int')
+  ) %>%
+  select(!ends_with("_0"))
+
+sample %>%
+  ggplot(aes(range, detection, color = as.factor(turbines))) +
+  geom_jitter(width = 0, height = .02) +
+  geom_line(aes(y = .pred_1),       plot_dat, size = 1) +
+  geom_line(aes(y = .pred_lower_1), plot_dat, linetype = 2) +
+  geom_line(aes(y = .pred_upper_1), plot_dat, linetype = 2) +
+  coord_cartesian(xlim = c(0, NA)) +
+  geom_point(aes(R50,   .5), R50_dat) +
+  geom_point(aes(lower, .5), R50_dat) +
+  geom_point(aes(upper, .5), R50_dat) +
+  theme_bw()
---
+++ R50_functions.R
@@ -0,0 +1,52 @@
+
+require(dplyr)
+require(parsnip)
+require(stringr)
+
+#' Finds the estimated range of the target for a given probability that the target will be detected.
+#' @param log_fit The fit of the regression as a parsnip model object
+#' @param prob The probability that the taget will be detected.
+#' @param turbines Are there turbinds? 1/0 or T/F
+range_from_prob <- function(prob, turbines, fit) {
+
+  prob_root <- function(range, turbines, log_fit, prob) {
+    coefs <- log_fit$fit %>% coef() %>% unname()
+    return(1 / (1 + exp(-(coefs[1] + coefs[2] * range +
+                            coefs[3] * turbines)))
+           - prob)
+  }
+
+  map2_dbl(turbines,
+           prob,
+           ~ uniroot(prob_root,
+                     interval = range(fit$fit$data$range),
+                     prob = .y,
+                     log_fit = fit,
+                     turbines = .x) %>%
+             `$`(root))
+}
+
+
+solve_R50_conf <- function(R50_mod, res=1000, interval = c(0,40)) {
+
+  get_conf <- function(dat, conf_type) {
+    conf_col <- str_glue(".pred_{conf_type}_1") %>% sym()
+
+    dat %>%
+      group_by(turbines) %>%
+      select(turbines, range, !!conf_col) %>%
+      slice_min(order_by=abs(!!conf_col - .5), n=1) %>%
+      select(turbines, "{conf_type}" := range) %>%
+      ungroup()
+  }
+
+  solve_dat <- tibble(turbines = c(rep(0, res), rep(1, res))) %>%
+    mutate(range = rep(seq(interval[1], interval[2], length.out = res), 2)) %>%
+    bind_cols(
+      suppressMessages(predict(R50_mod, new_data = ., type = 'conf_int'))) %>%
+    select(!ends_with("_0"))
+
+
+  return(get_conf(solve_dat, "lower") %>%
+           left_join(get_conf(solve_dat, "upper"), by = "turbines"))
+}
