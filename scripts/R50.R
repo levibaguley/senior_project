84bda1a067811c797a9a911100f974823e0f33a7
levibaguley <levi.baguley@gmail.com>
2020-07-22 11:24:51 -0600

    final report and quad chart

--- scripts/R50.R
+++ scripts/R50.R
@@ -1,25 +1,30 @@
 library(tidyverse)
 library(parsnip)
+library(here)
 
-source(here::here("scripts/solve_R50.R"))
+# get the function that will solve for R50
+source(here("scripts/solve_R50.R"))
 
 sample <-
-  read_csv(here::here("radarwindmillsampledata.csv")) %>%
+  read_csv(here("radarwindmillsampledata.csv")) %>%
   rename(detection = response,
          turbines = windmills)
 
+# create `parsnip` logistic model
 R50_mod <- logistic_reg() %>%
   set_engine("glm") %>%
   fit(as.factor(detection) ~ range + turbines, data = sample)
 
+# find the R50 estimates and confidence intervals
 R50_dat <-
   tibble(turbines = 0:1) %>%
-  mutate(R50 = solve_R50(R50_mod, turbines, "pred"),
+  mutate(R50   = solve_R50(R50_mod, turbines, "pred"),
          lower = solve_R50(R50_mod, turbines, "lower"),
          upper = solve_R50(R50_mod, turbines, "upper"))
 
+# create data to display the logistic model and the confidence intervals
 plot_dat <-
-  tibble(turbines = c(rep(0, 1000), rep(1, 1000))) %>%
+  tibble(turbines = rep(0:1, each=1000)) %>%
   mutate(range = rep(seq(0, 40, length.out = 1000), 2)) %>%
   bind_cols(predict(R50_mod, new_data = ., type = "prob"),
             predict(R50_mod, new_data = ., type = "conf_int")) %>%
@@ -28,11 +33,18 @@ plot_dat <-
 R50_plot <-
   plot_dat %>%
   ggplot(aes(range, color = as.factor(turbines))) +
+  # the main logistic curves
   geom_line(aes(y = .pred_1)) +
+  # the lower bounds
   geom_line(aes(y = .pred_lower_1), linetype = 2) +
+  # the upper bounds
   geom_line(aes(y = .pred_upper_1), linetype = 2) +
-  geom_jitter(aes(y = detection), sample, width = 0, height = .02) +
-  geom_point(aes(R50, .5, shape = "R50"), R50_dat) +
+  # the data points
+  geom_jitter(aes(y = detection),
+              data = sample, width = 0, height = .02) +
+  # R50 estimates
+  geom_point(aes(R50, .5, shape = "R50"),
+             data = R50_dat) +
   scale_shape_manual(NULL,
                      values = c("R50" = 8),
                      labels = expression(R[50])) +
--- scripts/R50_power.R
+++ scripts/R50_power.R
@@ -1,15 +1,21 @@
 library(pwr)
 
-R50_dat %>%
+# show the margins above and below the R50 point estimate
+conf_margins <-
+  R50_dat %>%
   mutate(across(c(lower, upper), ~ .x - R50, .names = "{col}_margin"))
 
+# create plot of power curves
 power_plot <-
+  # the effect sizes that will be ploted
   seq(.5, 2.5, .5) %>%
+  # run power calculations for each effect size and put it in a dataframe
   map_dfr(~ pwr.t.test(n = seq(2, 50, .05),
                        type = "two.sample",
                        d = .x,
                        alternative = "greater") %>%
             tidy() %>%
+            # keep track what effect size the calculations are for
             mutate(effect_size = as.factor(.x))) %>%
   mutate(effect_size = fct_rev(effect_size)) %>%
   ggplot(aes(n, power, color = effect_size)) +
@@ -20,5 +26,5 @@ power_plot <-
                                 " under different turbine conditions")),
        x = "Sample size (for each condition)",
        color = "Effect Size",
-       subtitle = expression("Effect size in this sample was" %~%2.5)) +
+       subtitle = "Effect size in this sample was about 2.5") +
   theme_bw()
